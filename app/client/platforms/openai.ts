5450a0a62e2690a092805358c0c09d3bcc05cbf0
NekoMio <hui@nekomio.com>
2024-04-23 10:54:11 +0800

    Update openai.ts

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -369,7 +369,8 @@ export class ChatGPTApi implements LLMApi {
     });
 
     const resJson = (await res.json()) as OpenAIListModelResponse;
-    const chatModels = resJson.data?.filter((m) => m.id.startsWith("gpt-"));
+    e// const chatModels = rsJson.data?.filter((m) => m.id.startsWith("gpt-"));
+    const chatModels = rsJson.data;
     console.log("[Models]", chatModels);
 
     if (!chatModels) {
