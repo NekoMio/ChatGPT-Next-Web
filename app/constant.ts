f331211231bb65c26f7c238b634f075b06618de6
NekoMio <hui@nekomio.com>
2024-04-23 10:35:55 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -145,6 +145,9 @@ const openaiModels = [
   "gpt-3.5-turbo-0125",
   "gpt-3.5-turbo-16k",
   "gpt-3.5-turbo-16k-0613",
+  "claude-3-sonnet-20240229",
+  "claude-3-opus-20240229",
+  "claude-3-haiku-20240307",
   "gpt-4",
   "gpt-4-0314",
   "gpt-4-0613",
@@ -169,9 +172,6 @@ const anthropicModels = [
   "claude-instant-1.2",
   "claude-2.0",
   "claude-2.1",
-  "claude-3-sonnet-20240229",
-  "claude-3-opus-20240229",
-  "claude-3-haiku-20240307",
 ];
 
 export const DEFAULT_MODELS = [
